5f1e809b44c117223c625ee1b55aeb5732e46113
H.C. Chen <hcchen5600@gmail.com>
2021-04-19 21:50:13 +0800

    save

--- bruteforce_time.py
+++ bruteforce_time.py
@@ -4,7 +4,7 @@ import time
 
 weight_limit = 3000
 
-for i in range(31):
+for i in range(24):  # 12:14 2021/04/03 was 31 but that takes too much time on my computer
     things = knapsack.generate_things(i)
     start = time.time()
     bruteforce(things, weight_limit)
